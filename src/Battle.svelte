a29d772b1606169feabd2d7511bc3e63cf3b641d
soshiharami <soshi0807sss@gmail.com>
2021-03-15 21:02:59 +0900

    feat: add send word

--- src/Battle.svelte
+++ src/Battle.svelte
@@ -6,27 +6,31 @@
   const nullSendWebSocket = { message: '', room: null, meta: 'null' };
   let enemyTypeWord = '';
   let userData = store.userData;
+  let matchData = store.matchData;
+  let words = word.word
 
   onMount(() => {
     store.subscribable((currentType) => {
-      const tmpEnemyTypeWord = JSON.parse(currentType || JSON.stringify(nullSendWebSocket)).message;
-      if ($userData.typeWord != tmpEnemyTypeWord) {
-        enemyTypeWord = tmpEnemyTypeWord;
+      const tmpEnemyData = JSON.parse(currentType || JSON.stringify(nullSendWebSocket));
+      if (tmpEnemyData.meta === "word"){
+        words = words.concat(tmpEnemyData.message)
+      } else if ($userData.typeWord != tmpEnemyData.message) {
+        enemyTypeWord = tmpEnemyData.message;
       }
     });
   });
 
   const checkString = () => {
-    if ($userData.typeWord == $userData.name) {
-      $userData.score += $userData.name.length * 10;
-      $userData.name = word.word[Math.floor(Math.random() * word.word.length)];
+    if ($userData.typeWord == $matchData.word) {
+      $userData.score += $matchData.word.length * 10;
+      $matchData.word = words[Math.floor(Math.random() * words.length)];
       $userData.typeWord = '';
     }
     $userData.score = $userData.score - 1;
     store.sendType(
       JSON.stringify({
         message: $userData.typeWord,
-        roomId: $userData.room,
+        roomId: $matchData.room,
         meta: 'message',
       }),
     );
@@ -34,12 +38,12 @@
 </script>
 
 <main>
-  <h5>{$userData.room}</h5>
-  <h1>{$userData.name}</h1>
+  <h5>{$matchData.room}</h5>
+  <h1>{$matchData.word}</h1>
   <h1>{$userData.typeWord}</h1>
   <h1>{enemyTypeWord}</h1>
   <label on:input="{checkString}">
-    <input type="text" bind:value="{$userData.typeWord}" />
+    <input type="text" bind:value="{$userData.typeWord}" oncopy="return false" onpaste="return false" oncontextmenu="return false" />
   </label>
   score {$userData.score}
 </main>
--- src/Title.svelte
+++ src/Title.svelte
@@ -2,14 +2,22 @@
   import store from 'src/store';
 
   let userData = store.userData;
+  let matchData = store.matchData;
+  let word: string;
 
   const join = () => {
     const sendMessage = {
       message: $userData.typeWord,
-      roomId: $userData.room,
+      roomId: $matchData.room,
       meta: 'join',
     };
+    const sendWord = {
+      message: word,
+      roomId: $matchData.room,
+      meta: 'word',
+    }
     store.sendType(JSON.stringify(sendMessage));
+    store.sendType(JSON.stringify(sendWord));
     $userData.isReady = false;
     $userData.isStart = true;
   };
@@ -18,7 +26,13 @@
 <main>
   <div>
     <label>
-      <input type="text" bind:value="{$userData.room}" />
+      <input type="number" bind:value="{$matchData.room}" />
+    </label>
+  </div>
+
+  <div>
+    <label>
+      <input type='text' bind:value="{word}">
     </label>
   </div>
   <button on:click="{join}"> 入る</button>
